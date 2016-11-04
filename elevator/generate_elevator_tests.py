2647c521d5c87bfd5771151cd15263d1c96bcf5b
Tim Menninger <tmenninger@caltech.edu>
2016-11-03 20:39:51 -0700

    fixed row change input in test vectors

--- elevator/generate_elevator_tests.py
+++ elevator/generate_elevator_tests.py
@@ -1,5 +1,6 @@
 # General imported things
-import os, math
+import math
+from sys import argv
 
 # Module with generic functions to write test vectors to files
 from test_vectors import *
@@ -11,6 +12,18 @@ from floor_patterns import *
 #
 #
 
+#
+# generate_test_vectors
+#
+# Generates test vectors for the elevator part of HW5 for EE119a.  It will
+# test as if the elevator moves up and down, and should show the display change
+# at each new "floor".
+#
+# Arguments: openfile (file) - The file to write test vectors to.
+# Returns:   Nothing
+#
+# Revisions: 11/03/16 - Tim Menninger: Created
+#
 def generate_test_vectors(openfile):
     '''
     Defines all of the test vectors and comments that will go into the file.
@@ -35,9 +48,33 @@ def generate_test_vectors(openfile):
     # there.  The order here shows the ground both starting on the second
     # row (first known row, so it displays ground after one clock) and not
     # so it spends some time before knowing it's on the ground floor
+    print_comment(openfile,
+        'We will start at the ground floor, then move up to floor 4, down to '
+      + 'subbasement, up to floor 3, then back to ground.  This will test '
+      + 'starting on ground with the mux row NOT a known row, moving to ground '
+      + 'from an unknown/searching state, and moving to ground while on a '
+      + 'row known to be ground (so it should change immediately).'
+    )
     floor_order = [ 2, 3, 4, 5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 4, 3, 2 ]
+    floor_names = [ 'subbasement',
+                    'basement',
+                    'ground floor',
+                    'floor 2',
+                    'floor 3',
+                    'floor 4' ]
+    chg = True
     for f in floor_order:
-        mux_row = on_floor(openfile, arrays[f], mux_row, 33, 3)
+        print_comment(openfile,
+            'Moving to ' + floor_names[f]
+        )
+        mux_row, chg = on_floor(openfile, arrays[f], mux_row, 60, 3, chg)
+
+    print_comment(openfile,
+        'Note that all possible state encodings have a state definition, so '
+      + 'we do not need to check or test for invalid states.  Also note that '
+      + 'we simply relay the input unless we are in a known state, so there '
+      + 'is no such thing as an invalid input.'
+    )
 
     return
 
@@ -47,7 +84,32 @@ def generate_test_vectors(openfile):
 #
 #
 
-def on_floor(openfile, light_array, mux_row, num_clocks, clocks_per_row):
+#
+# on_floor
+#
+# Generates test vectors as if it is on the floor designated by the light_array
+# argument.  It will start on the argued mux_row, and every clocks_per_row,
+# it will go on to print a test vector for the next row.  This will run for
+# num_clocks iterations.  This will alternate the value of chg between 0 and 1
+# on each new row.  It assumes that the argued file is open and ready to be
+# written to.  This runs for ceil(num_clocks / clocks_per_row) * clocks_per_row
+# iterations so as to not mess up the chg input.
+#
+# Arguments: openfile (file) - The file to write test vectors to
+#            light_array ([[int]]) - Designates the light patterns for each row
+#               where each value is a 0 or a 1
+#            mux_row (int) - The row to start on
+#            num_clocks (int) - The number of clocks to run for
+#            clocks_per_row (int) - The number of clocks spent displaying each
+#               row
+#            chg (bool) - The beginning value of chg (this is alternated
+#               immediately)
+# Returns:   mux_row (int) - The ending row being displayed
+#            chg (bool) - The ending value of chg
+#
+# Revisions: 11/03/16 - Tim Menninger: Created
+#
+def on_floor(openfile, light_array, mux_row, num_clocks, clocks_per_row, chg):
     '''
     Simulates the elevator being on a particular floor (designated by the light
     array) for some number of clocks.  Outputs which row of mux it is on.
@@ -76,10 +138,12 @@ def on_floor(openfile, light_array, mux_row, num_clocks, clocks_per_row):
             light_array_out = ground
 
         # Row has changed when clocks since last row change is 0
-        changed = (clocks == 0)
+        if (clocks == 0):
+            chg = not chg
+
         # Define left and right sides of test vector
-        inputs = light_array[mux_row]
-        outputs = light_array_out[mux_row] + [ int(changed) ]
+        inputs = ['.C.'] + light_array[mux_row] + [ int(chg) ]
+        outputs = light_array_out[mux_row]
         print_test_vector(openfile, inputs, outputs)
 
         # Update mux row and clocks since row change
@@ -87,7 +151,7 @@ def on_floor(openfile, light_array, mux_row, num_clocks, clocks_per_row):
         mux_row += (clocks == 0)
         mux_row %= 7 # 7 rows in 5x7 display
 
-    return mux_row
+    return mux_row, chg
 
 ###############################################################################
 #
@@ -96,21 +160,7 @@ def on_floor(openfile, light_array, mux_row, num_clocks, clocks_per_row):
 #
 
 if __name__ == '__main__':
-    # Copy original file to temporary file, which we will then copy back in
-    # to the original
-    origfile = open('elevator.abl', 'r')
-    copyfile = open('elevator_temp.abl', 'w')
-    copy_file(origfile, copyfile)
-    origfile.close()
-    copyfile.close()
-
-    origfile = open('elevator_temp.abl', 'r')
-    newfile  = open('elevator.abl', 'w')
-    insert_test_vectors(origfile, newfile, generate_test_vectors)
-    origfile.close()
-    newfile.close()
-
-    # Remove the directory, which is in this same directory
-    path = os.path.realpath(__file__)
-    path = '/'.join(path.split('/')[:-1])
-    os.remove(os.path.join(path, 'elevator_temp.abl'))
+    if (len(argv) != 2):
+        print ('usage: python remove_test_vectors.py [filename]')
+    # Insert test vectors into file
+    insert_test_vectors(argv[1], generate_test_vectors)
