5ea767fde128129270f63cfa2186e730d828c22d
Tim Menninger <tmenninger@caltech.edu>
2016-12-01 20:25:54 -0800

    moving to mac

--- VHDL/BCD2B8/BCD2B8.vhd
+++ VHDL/BCD2B8/BCD2B8.vhd
@@ -12,6 +12,8 @@
 --  B1, as there is no potential for carry from the B0 bit).  Note that B7 is always 0--there is
 --  no set of inputs that cause a carry to spill to bit 7.
 --
+--  Entities: BCD2binary8 - Converts 2-digit BCD to 8-bit binary
+--
 --  Inputs: BCD[7..0] - A two-digit BCD value, where each nibble represents one decimal value,
 --              the high bit being the high bit of the tens digit (which is the high nibble)
 --
@@ -36,7 +38,7 @@ entity BCD2binary8 is
         BCD         : in  std_logic_vector(7 downto 0);  -- BCD value
         B           : out std_logic_vector(7 downto 0)   -- value in binary
     );
-end entity;
+end BCD2binary8;
 
 ---------------------------------------------------------------------------------------------------
 --
@@ -61,6 +63,9 @@ begin
     -- is the XOR of {the bit immediately to its right, and any set bits in the binary
     -- representation shown above the process}.  In these, the "factors" referred to in
     -- comments are terms XOR'd together.
+
+    -- Bits 0..3 were defined by looking for patterns.  Refer to file header for methodology
+    -- in finding the patterns.  They correspond to the ones digit of BCD
     B(0) <= BCD(0);
     B(1) <= BCD(1) xor BCD(4);
     B(2) <= (BCD(1) and BCD(4)) xor BCD(2) xor BCD(5);  -- Both B(1) factors causes carry
@@ -69,27 +74,28 @@ begin
             ((BCD(1) and BCD(4)) and BCD(5)))
         xor BCD(3) xor BCD(4) xor BCD(6);
 
-    -- Rest of these were done with Karnaugh maps, and correspond to the tens BCD digit
+    -- BIts 4..7 were defined with the help of Karnaugh maps, and correspond to the tens BCD digit
     B(4) <= (BCD(7) and not BCD(4)) or
             (not BCD(6) and BCD(5) and not BCD(4)) or
             (BCD(5) and not BCD(4) and not BCD(3) and not BCD(2)) or
-            (BCD(6) and not BCD(5) and not BCD(4)) or
             (BCD(6) and not BCD(5) and BCD(3)) or
-            (BCD(7) and not BCD(3) and not BCD(2)) or
             (BCD(7) and BCD(2) and not BCD(1)) or
             (not BCD(7) and not BCD(6) and not BCD(5) and BCD(4) and BCD(3)) or
-            (not BCD(6) and BCD(5) and not BCD(3) and not BCD(2) and not BCD(1)) or
-            (not BCD(7) and not BCD(6) and not BCD(5) and BCD(4) and BCD(2) and BCD(1));
+            (not BCD(7) and not BCD(6) and not BCD(5) and BCD(4) and BCD(2) and BCD(1)) or
+            (BCD(6) and not BCD(5) and BCD(4)) or
+            (BCD(7) and BCD(4) and not BCD(3) and not BCD(2)) or
+            (not BCD(6) and BCD(5) and BCD(4) and not BCD(3) and not BCD(2) and not BCD(1));
     B(5) <= (BCD(6) and not BCD(5)) or
             (BCD(7) and BCD(4) and BCD(3)) or
             (BCD(7) and BCD(4) and BCD(2) and BCD(1)) or
             (not BCD(6) and BCD(5) and BCD(4) and BCD(2)) or
-            (not BCD(6) and BCD(5) and BCD(4) and BCD(1));
+            (not BCD(6) and BCD(5) and BCD(4) and BCD(1)) or
+            (BCD(6) and not BCD(4) and not BCD(3) and not BCD(2));
     B(6) <= (BCD(7)) or
             (BCD(6) and BCD(5) and BCD(4)) or
             (BCD(6) and BCD(5) and BCD(3)) or
             (BCD(6) and BCD(5) and BCD(2));
     B(7) <= '0';
-end architecture;
+end converter;
 
 ---------------------------------------------------------------------------------------------------
